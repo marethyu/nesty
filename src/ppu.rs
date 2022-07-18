1895c2fcb771e08cb7479cb2e00ee45dca3f936d
marethyu <codingexpert123@gmail.com>
2022-07-17 20:22:06 -0700

    Implement internal registers for PPU

--- src/ppu.rs
+++ src/ppu.rs
@@ -95,6 +95,31 @@ bitfield! {
 }
 
 /*
+15-bit register that holds vram address for use in scrolling logic
+Format: yyy NN YYYYY XXXXX
+*/
+bitfield! {
+    #[derive(Clone, Copy, PartialEq, Eq)]
+    pub struct VRAMAddress(pub u16): Debug {
+        /* value for this 15 bit field */
+        pub raw: u16 @ 0..=14,
+
+        /* x position within a namtable (range: 0-31) */
+        pub coarse_x: u8 @ 0..=4,   // XXXXX
+
+        /* y position within a nametable (range: 0-29) */
+        pub coarse_y: u8 @ 5..=9,   // YYYYY
+
+        /* nametable select */
+        pub nametable_x: bool @ 10, // N
+        pub nametable_y: bool @ 11, // N
+
+        /* y position within a tile */
+        pub fine_y: u8 @ 12..=14    // yyy
+    }
+}
+
+/*
 PPU Memory Map (14bit buswidth, 0-3FFFh)
   0000h-0FFFh   Pattern Table 0 (4K) (256 Tiles)
   1000h-1FFFh   Pattern Table 1 (4K) (256 Tiles)
@@ -130,17 +155,15 @@ pub struct PPU {
     // OAM ADDRESS ($2003)
     oam_addr: u8,
 
-    // PPU SCROLL ($2005)
-    scrollx: u8,
-    scrolly: u8,
-
-    // PPU ADDRESS ($2006)
-    vram_address: u16,
-
     // PPU DATA ($2007)
     prev_data: u8, /* AKA IO bus for open bus implementation */
 
     oam: Vec<u8>,
+
+    // Internal registers
+    vram_address: VRAMAddress,
+    temp_vram_address: VRAMAddress,
+    fine_x: u8,
     addr_latch: bool,
 
     scanline: i32,
@@ -168,14 +191,13 @@ impl PPU {
 
             oam_addr: 0,
 
-            scrollx: 0,
-            scrolly: 0,
-
-            vram_address: 0,
-
             prev_data: 0,
 
             oam: vec![0; OAM_SIZE],
+
+            vram_address: VRAMAddress(0),
+            temp_vram_address: VRAMAddress(0),
+            fine_x: 0,
             addr_latch: false,
 
             scanline: 0,
@@ -200,13 +222,11 @@ impl PPU {
 
         self.oam_addr = 0;
 
-        self.scrollx = 0;
-        self.scrolly = 0;
-
-        self.vram_address = 0;
-
         self.prev_data = 0;
 
+        self.vram_address.set_raw(0);
+        self.temp_vram_address.set_raw(0);
+        self.fine_x = 0;
         self.addr_latch = false;
 
         self.scanline = 0;
@@ -222,19 +242,46 @@ impl PPU {
                 if self.scanline == -1 && self.cycle == 1 {
                     self.status.set_vblank(false); // clear vblank before rendering
                     self.status.set_sprite_zero_hit(false);
+                }
 
-                    // ntx might change after scrolling so reset
-                    self.control.set_nametable_x(false);
-                    // TODO is resetting nty also necessary?!?
-                    // self.nametable_y = false;
+                if self.scanline == -1 && (self.cycle >= 280 && self.cycle <= 304) {
+                    // Vertical update
+                    // v: GHIA.BC DEF..... <- t: GHIA.BC DEF.....
+                    if self.rendering_on() {
+			            self.vram_address.set_coarse_y(self.temp_vram_address.coarse_y());
+                        self.vram_address.set_nametable_y(self.temp_vram_address.nametable_y());
+                        self.vram_address.set_fine_y(self.temp_vram_address.fine_y());
+                    }
                 }
 
                 if self.scanline == 0 && self.cycle == 0 && self.odd_frame {
                     self.cycle += 1; // skip if odd
                 }
 
+                if self.scanline >= 0 && self.cycle % 8 == 0 && self.cycle <= 256 {
+                    self.inc_scrollx();
+                }
+
+                if self.scanline >= 0 && self.cycle == 256 {
+                    // TODO is it necessary?
+                    // self.inc_scrolly();
+                }
+
+                if self.scanline >= 0 && self.cycle == 257 {
+                    // Horizontal update
+                    // v: ....A.. ...BCDEF <- t: ....A.. ...BCDEF
+                    if self.rendering_on() {
+                        self.vram_address.set_coarse_x(self.temp_vram_address.coarse_x());
+                        self.vram_address.set_nametable_x(self.temp_vram_address.nametable_x());
+                    }
+                }
+
                 if self.scanline >= 0 && self.cycle == 258 {
                     self.render_scanline();
+                }
+
+                if self.scanline >= 0 && (self.cycle == 328 && self.cycle == 336) {
+                    self.inc_scrollx();
                 }
             }
             240 => {      /* Post render scanline */
@@ -271,14 +318,17 @@ impl PPU {
     }
 
     fn render_bkgd(&mut self) {
-        let nn = ((self.control.nametable_y() as u16) << 1) | (self.control.nametable_x() as u16);
+        let nn = ((self.vram_address.nametable_y() as u16) << 1) | (self.vram_address.nametable_x() as u16);
 
         let mut base_nt_addr: u16 = NT_START + nn * (NAMETABLE_SIZE as u16);
         let mut base_attr_addr: u16 = AT_START + nn * (NAMETABLE_SIZE as u16);
 
         let mut scx_flag = false; // it will be true after base addresses' ntx bit get toggled
 
-        let mut actual_screen_y = (self.scanline as usize) + (self.scrolly as usize);
+        let scrollx = self.vram_address.coarse_x() * 8 + self.fine_x;
+        let scrolly = self.vram_address.coarse_y() * 8 + self.vram_address.fine_y();
+
+        let mut actual_screen_y = (self.scanline as usize) + (scrolly as usize);
 
         // Yuh oh... we need to fix base addresses
         if actual_screen_y >= HEIGHT {
@@ -296,7 +346,7 @@ impl PPU {
         let pattstart = if self.control.bkgd_pattern() { PT1_START } else { PT0_START };
 
         for screen_x in 0..WIDTH {
-            let mut actual_screen_x = screen_x + (self.scrollx as usize);
+            let mut actual_screen_x = screen_x + (scrollx as usize);
 
             // Yuh oh... we need to fix base addresses
             if actual_screen_x >= WIDTH {
@@ -581,24 +631,25 @@ impl PPU {
                 data = self.prev_data;
 
                 self.status.set_vblank(false);
+                // w:                  <- 0
                 self.addr_latch = false;
             }
             0x4 => { // OAM DATA
                 data = self.oam[self.oam_addr as usize];
             }
             0x7 => { // PPU DATA
+                let mut addr: u16 = self.vram_address.raw();
+
                 data = self.prev_data; // reads from nametable are delayed by one cycle
-                self.prev_data = self.read_byte(self.vram_address);
+                self.prev_data = self.read_byte(addr);
 
                 // the current address was in the palette range
-                if self.vram_address >= 0x3F00 {
+                if addr >= 0x3F00 {
                     data = self.prev_data;
                 }
 
-                self.vram_address += if self.control.vram_increment_downwards() { 32 } else { 1 };
-                if self.vram_address > 0x3FFF {
-                    self.vram_address &= 0b11111111111111;
-                }
+                addr += if self.control.vram_increment_downwards() { 32 } else { 1 };
+                self.vram_address.set_raw(addr);
             }
             _ => {}
         }
@@ -609,6 +660,11 @@ impl PPU {
         match register {
             0x0 => { // PPU CONTROL
                 self.control.set_raw(data);
+
+                //    yyyNNYY YYYXXXXX
+                // t: ...GH.. ........ <- d: ......GH
+                self.temp_vram_address.set_nametable_x(self.control.nametable_x());
+                self.temp_vram_address.set_nametable_y(self.control.nametable_y());
             }
             0x1 => { // PPU MASK
                 self.mask.set_raw(data);
@@ -622,36 +678,94 @@ impl PPU {
             }
             0x5 => { // PPU SCROLL
                 if !self.addr_latch {
-                    self.scrollx = data;
+                    // scroll x
+                    //    yyyNNYY YYYXXXXX
+                    // t: ....... ...ABCDE <- d: ABCDE...
+                    self.temp_vram_address.set_coarse_x((data & 0b11111000) >> 3);
+                    // x:              FGH <- d: .....FGH
+                    self.fine_x                        = data & 0b00000111;
                 } else {
-                    self.scrolly = data;
+                    // scroll y
+                    // t: FGH..AB CDE..... <- d: ABCDEFGH
+                    self.temp_vram_address.set_coarse_y((data & 0b11111000) >> 3);
+                    self.temp_vram_address.set_fine_y   (data & 0b00000111);
                 }
+
                 self.addr_latch = !self.addr_latch;
             }
             0x6 => { // PPU ADDRESS
                 if !self.addr_latch {
                     // high byte
-                    self.vram_address = ((data as u16) << 8) | (self.vram_address & 0b0000000011111111);
+                    //    yyyNNYY YYYXXXXX
+                    // t: .CDEFGH ........ <- d: ..CDEFGH
+                    //        <unused>     <- d: AB......
+                    // t: Z...... ........ <- 0 (bit Z is cleared)
+                    self.temp_vram_address.set_raw((((data as u16) & 0b00111111) << 8) |
+                                                   (self.temp_vram_address.raw() & 0b000000011111111));
                 } else {
                     // low byte
-                    self.vram_address = (self.vram_address & 0b1111111100000000) | (data as u16);
-                }
-
-                if self.vram_address > 0x3FFF {
-                    self.vram_address &= 0b11111111111111;
+                    // t: ....... ABCDEFGH <- d: ABCDEFGH
+                    self.temp_vram_address.set_raw((self.temp_vram_address.raw() & 0b111111100000000) |
+                                                   (data as u16));
+                    // v: <...all bits...> <- t: <...all bits...>
+                    self.vram_address.set_raw(self.temp_vram_address.raw());
                 }
 
                 self.addr_latch = !self.addr_latch;
             }
             0x7 => { // PPU DATA
-                self.write_byte(self.vram_address, data);
+                let mut addr: u16 = self.vram_address.raw();
 
-                self.vram_address += if self.control.vram_increment_downwards() { 32 } else { 1 };
-                if self.vram_address > 0x3FFF {
-                    self.vram_address &= 0b11111111111111;
-                }
+                self.write_byte(addr, data);
+
+                addr += if self.control.vram_increment_downwards() { 32 } else { 1 };
+                self.vram_address.set_raw(addr);
             }
             _ => {}
+        }
+    }
+
+    fn inc_scrolly(&mut self) {
+        if !self.rendering_on() {
+            return;
+        }
+
+        let fine_y = self.vram_address.fine_y();
+        // Move downwards in terms of fine y not coarse y
+        if fine_y == 8 {
+            self.vram_address.set_fine_y(0);
+
+            let coarse_y = self.vram_address.coarse_y();
+            // move down to next tile (last 2 rows contains attribute table data)
+            if coarse_y == 29 {
+                self.vram_address.set_coarse_y(0);
+
+                // move down to next nametable
+                self.vram_address.set_nametable_y(!self.vram_address.nametable_y());
+            } else {
+                if coarse_y == 31 {
+                    self.vram_address.set_coarse_y(0);
+                } else {
+                    self.vram_address.set_coarse_y(coarse_y + 1);
+                }
+            }
+        } else {
+            self.vram_address.set_fine_y(fine_y + 1);
+        }
+    }
+
+    fn inc_scrollx(&mut self) {
+        if !self.rendering_on() {
+            return;
+        }
+
+        let coarse_x = self.vram_address.coarse_x();
+
+        if coarse_x == 31 {
+            self.vram_address.set_coarse_x(0);
+            self.vram_address.set_nametable_x(!self.vram_address.nametable_x());
+        } else {
+            self.vram_address.set_coarse_x(coarse_x + 1);
         }
     }
 
