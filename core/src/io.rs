b5d684462495f8404b8fc61f5d1338c20001da47
marethyu <marethyu@users.noreply.github.com>
2022-07-21 07:20:58 +0000

    deploy: 9bdd2eeec016d91e4358b300f090878418fa89b5

--- core/src/io.rs
+++
@@ -1,6 +0,0 @@
-pub trait IO {
-    fn read_byte(&mut self, addr: u16) -> u8;
-    fn read_word(&mut self, addr: u16) -> u16;
-    fn write_byte(&mut self, addr: u16, data: u8);
-    fn write_word(&mut self, addr: u16, data: u16);
-}
